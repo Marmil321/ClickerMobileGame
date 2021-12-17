96aeb8c944310ade94147e24e91f1e2c4591abd8
MarcusMilsem <Emilio.iversen@gmail.com>
2021-12-17 20:16:53 +0100

    Upgrade Requirements (almost working)

--- Assets/Upgrades.cs
+++ Assets/Upgrades.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +7,27 @@ using TMPro;
 public class Upgrades : MonoBehaviour
 {
     [SerializeField]public GameObject[] obj;
-    public bool classTest;
 
     public UpgradeManger manager;
+    //public Requirements req;
+
+    public Vector2[] placement;
+    public TMP_Text[] text;
+    public GameObject[] test;
+
+    Requirements req = new Requirements();
 
     private void Start()
     {
+        //req = GetComponent<Requirements>();
         manager = FindObjectOfType<UpgradeManger>();
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            print(Getlist());
+            print(GetList());
+
         }
     }
     public void CheckReq()
@@ -26,6 +35,13 @@ public class Upgrades : MonoBehaviour
         StateManager state = FindObjectOfType<StateManager>();
         InventoryManager inv = FindObjectOfType<InventoryManager>();
 
+        for(int i = 0; i < GetList().Count; i++)
+        {
+            print(i);
+            test[i] = Instantiate(GetList()[i].material, placement[i], Quaternion.identity);
+            //test[i].GetComponentInChildren<TMP_Text>().text = GetList()[i].cost.ToString();
+            //test[i].transform.SetParent(this.transform);
+        }
 
         switch (state.activeState)
         {
@@ -70,46 +86,36 @@ public class Upgrades : MonoBehaviour
                 break;
         }
     }
-    List<Requirements> Getlist()
+    public List<Requirements> GetList()
     {
         List<Requirements> list = new List<Requirements>();
-        Requirements req = new Requirements();
 
-        for(int i = 0; i < manager.reqs.Length; i++)
+        for (int i = 0; i < manager.reqs.Length; i++)
         {
-            if(manager.reqs[i] > 0)
+            if (manager.reqs[i] > 0)
             {
-                //list.Add(new Requirements() { material = req.GetMaterial(i) ,cost = manager.reqs[i] });
+                list.Add(new Requirements() { material = req.GetMaterial(i, obj), cost = manager.reqs[i] });
             }
-            //print(list[i].material);
+            print(list[i].material);
         }
 
-        print(req.testPrint());
-        //list.Add(new Requirements() {material =  obj, cost = 100});
-        
         return list;
     }
-}
-public class Requirements : MonoBehaviour
-{
-    public GameObject material;
-    public int cost;
 
-    Upgrades upgrade = new Upgrades();
-
-    public GameObject GetMaterial(int i)
+    public class Requirements
     {
-        switch (i)
+        public GameObject material;
+        public int cost;
+        public GameObject GetMaterial(int i, GameObject[] obj)
         {
-            default:
-            case 0: return upgrade.obj[0];
-            case 1: return upgrade.obj[1];
-            case 2: return upgrade.obj[2];
+            switch (i)
+            {
+                default:
+                case 0: return obj[0];
+                case 1: return obj[1];
+                case 2: return obj[2];
+            }
         }
     }
-    public bool testPrint()
-    {
-        upgrade.classTest = false;
-        return upgrade.classTest;
-    }
 }
+
