4db3bad099034f105f85e1748321ea7dd8eda1df
MarcusMilsem <Emilio.iversen@gmail.com>
2022-01-04 23:37:52 +0100

    almost nothing

--- Assets/DebugScript.cs
+++ Assets/DebugScript.cs
@@ -7,6 +7,7 @@ public class DebugScript : MonoBehaviour
     public void Set9999()
     {
         PlayerPrefs.SetInt("StoneAmount", 9999);
+        FindObjectOfType<InventoryManager>().Add("IronAmount", 100000);
         FindObjectOfType<InventoryManager>().Add("WoodAmount", 100000);
         FindObjectOfType<InventoryManager>().Add("StickAmount",1000);
         FindObjectOfType<InventoryManager>().Add("CoalAmount", 1000);
